eed310e6bf98f0d2fd6933c5f9448a7853461631
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2024-03-25 16:52:25 +0100

    Début de liaison avec la BD distante

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:allo/UI/demandes_ui.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://xyzcompany.supabase.co',
+    anonKey: 'public-anon-key',
+  );
+
+  runApp(MyApp());
 }
+
+// Get a reference your Supabase client
+final supabase = Supabase.instance.client;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
