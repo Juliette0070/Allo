b9df01a60948dbcd937b04ba93144c2d68da1ded
Juliette Rousselet <juliette.rousselet@etu.univ-orleans.fr>
2024-03-26 13:51:15 +0100

    le dark/light mode fonctionne correctement

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,104 @@
+import 'package:allo/mytheme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "All'o",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+          create: (_) {
+            SettingViewModel settingViewModel = SettingViewModel();
+            return settingViewModel;
+        },
+      child: Consumer<SettingViewModel>(
+        builder: (context,SettingViewModel notifier,child){
+          return MaterialApp(
+            title: "All'o",
+            theme: notifier.isDark ? MyTheme.dark():MyTheme.light(),
+            home: const MyHomePage(title: "All'o"),
+          );
+        },
       ),
-      home: const MyHomePage(title: "All'o"),
+    );
+  }
+}
+
+class SettingViewModel extends ChangeNotifier{
+  late bool _isDark;
+  late SettingRepository _settingRepository;
+  bool get isDark => _isDark;
+  SettingViewModel() {
+    _isDark = false;
+    _settingRepository = SettingRepository();
+    getSettings();
+  }
+  //Switching the themes
+  set isDark(bool value) {
+    _isDark = value;
+    _settingRepository.saveSettings(value);
+    notifyListeners();
+  }
+  getSettings() async {
+    _isDark = await _settingRepository.getSettings();
+    notifyListeners();
+  }
+}
+
+class SettingRepository{
+  static const THEME_KEY = "darkMode";
+
+  saveSettings(bool value) async {
+    SharedPreferences sharedPreferences = await
+    SharedPreferences.getInstance();
+    sharedPreferences.setBool(THEME_KEY, value);
+  }
+
+  Future<bool> getSettings() async {
+    SharedPreferences sharedPreferences = await
+    SharedPreferences.getInstance();
+    return sharedPreferences.getBool(THEME_KEY) ?? false;
+  }
+}
+
+class WidgetSettings extends StatefulWidget{
+  const WidgetSettings({super.key});
+  @override
+  State<WidgetSettings> createState() => _EcranSettingsState();
+}
+
+class _EcranSettingsState extends State<WidgetSettings>{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: SettingsList(
+        darkTheme: SettingsThemeData(
+            settingsListBackground: MyTheme.dark().scaffoldBackgroundColor,
+            settingsSectionBackground: MyTheme.dark().scaffoldBackgroundColor
+        ),
+        lightTheme: SettingsThemeData(
+            settingsListBackground: MyTheme.light().scaffoldBackgroundColor,
+            settingsSectionBackground: MyTheme.light().scaffoldBackgroundColor
+        ),
+        sections: [
+          SettingsSection(
+              title: const Text('Theme'),
+              tiles: [
+                SettingsTile.switchTile(
+                  initialValue: context.watch<SettingViewModel>().isDark,
+                  onToggle: (bool value) {context.read<SettingViewModel>().isDark=value;},
+                  title: const Text('Dark mode'),
+                  leading: const Icon(Icons.invert_colors),)
+              ])
+        ],
+      ),
     );
   }
 }
@@ -54,7 +136,7 @@ class _MyHomePageState extends State<MyHomePage> {
   static final List<Widget> _widgetOptions = [
     _WidgetDemandes(),
     _WidgetMateriel(),
-    _WidgetSettings()
+    WidgetSettings()
   ];
 
   void _onItemTapped(int index){
